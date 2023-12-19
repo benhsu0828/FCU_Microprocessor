23966a6a4946b5282aa97ca0f0d0aea359d8dca2
benhsu0828 <97419397+benhsu0828@users.noreply.github.com>
2023-12-19 22:27:30 +0800

    Update main.c

--- onsite3/Q1/SampleCode/Nu-LB-NUC140/lab11_2/main.c
+++ onsite3/Q1/SampleCode/Nu-LB-NUC140/lab11_2/main.c
@@ -19,7 +19,7 @@ volatile uint8_t KEY_Flag;
 volatile uint32_t index_key_scan;
 volatile uint32_t index0_5ms, cnt0_5ms, cnt0_1s, cnt0_100ms;
 volatile uint32_t index1_5ms, cnt1_5ms, cnt1_1s, cnt1_100ms;
-volatile uint32_t count_Line = 0; 
+volatile uint32_t count_Line = 0;
 volatile uint32_t count_Char;
 volatile uint8_t u8ADF;
 volatile uint32_t u32ADCvalue;
@@ -33,7 +33,7 @@ volatile int ball_diry = 1;
 volatile int pad_x = 0;
 volatile int pad_y = 56;
 volatile char RX_buffer[RXBUFSIZE];
-volatile int digit[4] = {0,0,0,0};
+volatile int digit[4] = {0, 0, 0, 0};
 volatile int resultFlag = 0;
 volatile int touchFlag = 0;
 char line0[16] = "               ";
@@ -41,64 +41,82 @@ char line1[16] = "               ";
 char line2[16] = "               ";
 char line3[16] = "               ";
 unsigned char pad[32] = {
-    0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
-    };
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 unsigned char ball[8] = {
-    0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
-    };
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-void EINT1_IRQHandler(void)//PB15
+void EINT1_IRQHandler(void) //PB15
 {
-    GPIO_CLR_INT_FLAG(PB, BIT15);  // Clear GPIO interrupt flag
+    GPIO_CLR_INT_FLAG(PB, BIT15); // Clear GPIO interrupt flag
 }
 
-void TMR0_IRQHandler(void){
-    if (cnt0_5ms % 20 == 0) 
-	{
-		digit[2]++;
-		if(digit[2] == 10){
-				digit[2] = 0;
-				digit[3]++;
-     }
-		if(digit[3] == 6){
-				digit[3] = 0;
-     }
-		cnt0_100ms++;
+void TMR0_IRQHandler(void)
+{
+    if (cnt0_5ms % 20 == 0)
+    {
+        digit[2]++;
+        if (digit[2] == 10)
+        {
+            digit[2] = 0;
+            digit[3]++;
+        }
+        if (digit[3] == 6)
+        {
+            digit[3] = 0;
+        }
+        cnt0_100ms++;
     }
-    if(cnt0_5ms % 200 == 0){
-			
-		cnt0_1s++;
-	}
+    if (cnt0_5ms % 200 == 0)
+    {
+
+        cnt0_1s++;
+    }
     cnt0_5ms++;
     TIMER_ClearIntFlag(TIMER0); // Clear Timer0 time-out interrupt flag
 }
 
 void TMR1_IRQHandler(void)
-{// for keypad and seven segment
-	if (cnt1_5ms % 20 == 0) 
-	{
-		cnt1_100ms++;
-		index_key_scan = cnt1_100ms++ % 3;
-		if (index_key_scan == 0)
-		{
-			PA0=1; PA1=1; PA2=1; PA3=1; PA4=1; PA5=0;
-		}
-		if (index_key_scan == 1)
-		{
-			PA0=1; PA1=1; PA2=1; PA3=1; PA4=0; PA5=1;
-		}
-		if (index_key_scan == 2)
-		{
-			PA0=1; PA1=1; PA2=1; PA3=0; PA4=1; PA5=1;
-		}
-		NVIC_EnableIRQ(GPAB_IRQn);
-	}
-	if(cnt1_5ms % 200 == 0){
-		cnt1_1s++;
-	}
+{ // for keypad and seven segment
+    if (cnt1_5ms % 20 == 0)
+    {
+        cnt1_100ms++;
+        index_key_scan = cnt1_100ms++ % 3;
+        if (index_key_scan == 0)
+        {
+            PA0 = 1;
+            PA1 = 1;
+            PA2 = 1;
+            PA3 = 1;
+            PA4 = 1;
+            PA5 = 0;
+        }
+        if (index_key_scan == 1)
+        {
+            PA0 = 1;
+            PA1 = 1;
+            PA2 = 1;
+            PA3 = 1;
+            PA4 = 0;
+            PA5 = 1;
+        }
+        if (index_key_scan == 2)
+        {
+            PA0 = 1;
+            PA1 = 1;
+            PA2 = 1;
+            PA3 = 0;
+            PA4 = 1;
+            PA5 = 1;
+        }
+        NVIC_EnableIRQ(GPAB_IRQn);
+    }
+    if (cnt1_5ms % 200 == 0)
+    {
+        cnt1_1s++;
+    }
     cnt1_5ms++;
     CloseSevenSegment();
-    ShowSevenSegment(index1_5ms,digit[index1_5ms]);
+    ShowSevenSegment(index1_5ms, digit[index1_5ms]);
     index1_5ms = cnt1_5ms % 4;
     TIMER_ClearIntFlag(TIMER1); // Clear Timer1 time-out interrupt flag
 }
@@ -107,45 +125,93 @@ void GPAB_IRQHandler(void)
 {
     NVIC_DisableIRQ(GPAB_IRQn);
 
-    if (PA->ISRC & BIT0) {        // check if PA0 interrupt occurred
-        PA0=1;
-        PA->ISRC |= BIT0;         // clear PA0 interrupt status
-    if (PA3==0) { KEY_Flag =3; PA3=1;cnt1_1s=0;}
-    if (PA4==0) { KEY_Flag =6; PA4=1;cnt1_1s=0;}
-    if (PA5==0) { KEY_Flag =9; PA5=1;cnt1_1s=0;}
-    return;			
-    } 
-    if (PA->ISRC & BIT1) { // check if PA1 interrupt occurred
-            PA1=1;
-        PA->ISRC |= BIT1;         // clear PA1 interrupt status  
-    if (PA3==0) { KEY_Flag =2; PA3=1;cnt1_1s=0;}
-    if (PA4==0) { KEY_Flag =5; PA4=1;cnt1_1s=0;}
-    if (PA5==0) { KEY_Flag =8; PA5=1;cnt1_1s=0;} 
-    return;				
-    } 
-    if (PA->ISRC & BIT2) { // check if PB14 interrupt occurred
-            PA2=1;
-        PA->ISRC |= BIT2;         // clear PA interrupt status  feeddog();Leave_PowerDown();
-    if (PA3==0) { KEY_Flag =1; PA3=1;cnt1_1s=0;}
-    if (PA4==0) { KEY_Flag =4; PA4=1;cnt1_1s=0;}
-    if (PA5==0) { KEY_Flag =7; PA5=1;cnt1_1s=0;}
-    return;				
-    }                     // else it is unexpected interrupts
-    PA->ISRC = PA->ISRC;	      // clear all GPB pins
+    if (PA->ISRC & BIT0)
+    { // check if PA0 interrupt occurred
+        PA0 = 1;
+        PA->ISRC |= BIT0; // clear PA0 interrupt status
+        if (PA3 == 0)
+        {
+            KEY_Flag = 3;
+            PA3 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA4 == 0)
+        {
+            KEY_Flag = 6;
+            PA4 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA5 == 0)
+        {
+            KEY_Flag = 9;
+            PA5 = 1;
+            cnt1_1s = 0;
+        }
+        return;
+    }
+    if (PA->ISRC & BIT1)
+    { // check if PA1 interrupt occurred
+        PA1 = 1;
+        PA->ISRC |= BIT1; // clear PA1 interrupt status
+        if (PA3 == 0)
+        {
+            KEY_Flag = 2;
+            PA3 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA4 == 0)
+        {
+            KEY_Flag = 5;
+            PA4 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA5 == 0)
+        {
+            KEY_Flag = 8;
+            PA5 = 1;
+            cnt1_1s = 0;
+        }
+        return;
+    }
+    if (PA->ISRC & BIT2)
+    { // check if PB14 interrupt occurred
+        PA2 = 1;
+        PA->ISRC |= BIT2; // clear PA interrupt status  feeddog();Leave_PowerDown();
+        if (PA3 == 0)
+        {
+            KEY_Flag = 1;
+            PA3 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA4 == 0)
+        {
+            KEY_Flag = 4;
+            PA4 = 1;
+            cnt1_1s = 0;
+        }
+        if (PA5 == 0)
+        {
+            KEY_Flag = 7;
+            PA5 = 1;
+            cnt1_1s = 0;
+        }
+        return;
+    }                    // else it is unexpected interrupts
+    PA->ISRC = PA->ISRC; // clear all GPB pins
 }
 
 // void UART02_IRQHandler(void)
 // {
 // 	  uint32_t u32IntSts= UART0->ISR; // UART interrupt status bit
 // 		uint8_t c;
-		
+
 //     if(u32IntSts & UART_IS_RX_READY(UART1)){
 // 			while (!(UART0->FSR & UART_FSR_RX_EMPTY_Msk)){ // check RX is not empty
 // 				c = UART_READ(UART0);
 // 				if (c!= '0') {
-					
+
 // 				}
-// 			}		
+// 			}
 //     }
 // }
 
@@ -156,37 +222,41 @@ void ADC_IRQHandler(void)
     // Get ADC conversion finish interrupt flag
     u32Flag = ADC_GET_INT_FLAG(ADC, ADC_ADF_INT);
 
-    if(u32Flag & ADC_ADF_INT)u8ADF = 1;
-    
+    if (u32Flag & ADC_ADF_INT)
+        u8ADF = 1;
+
     ADC_CLR_INT_FLAG(ADC, u32Flag);
 }
 
-void getVR(){
+void getVR()
+{
     ADC_START_CONV(ADC);
     //remove sleep just start ADC interrupt
-    while (u8ADF == 0);
-		u32ADCvalue = ADC_GET_CONVERSION_DATA(ADC, 7);
-		if(u32ADCvalue*96/4096 != pad_x){
-			//draw pad
-			//draw_Bmp32x8(pad_x,pad_y ,BG_COLOR,BG_COLOR , pad);
-			//draw_Bmp32x8(0,0 ,BG_COLOR,BG_COLOR , pad);
-			pad_x = u32ADCvalue*96/4096;//turn 0~4096 to 0~100
-			//draw pad
-			//draw_Bmp32x8(pad_x,pad_y ,FG_COLOR,BG_COLOR , pad);
-			//draw_Bmp32x8(0,0 ,FG_COLOR,BG_COLOR , pad);
-		}
-    //printf("ADC7 = %d, duty =%d\n", u32ADCvalue, duty);    
+    while (u8ADF == 0)
+        ;
+    u32ADCvalue = ADC_GET_CONVERSION_DATA(ADC, 7);
+    if (u32ADCvalue * 96 / 4096 != pad_x)
+    {
+        //draw pad
+        //draw_Bmp32x8(pad_x,pad_y ,BG_COLOR,BG_COLOR , pad);
+        //draw_Bmp32x8(0,0 ,BG_COLOR,BG_COLOR , pad);
+        pad_x = u32ADCvalue * 96 / 4096; //turn 0~4096 to 0~100
+                                         //draw pad
+                                         //draw_Bmp32x8(pad_x,pad_y ,FG_COLOR,BG_COLOR , pad);
+                                         //draw_Bmp32x8(0,0 ,FG_COLOR,BG_COLOR , pad);
+    }
+    //printf("ADC7 = %d, duty =%d\n", u32ADCvalue, duty);
     ADC_DisableInt(ADC, ADC_ADF_INT);
 }
 
 void Init_ALL(void)
 {
-    SYS_Init(); 
-	init_lcd(TRUE,TRUE);
+    SYS_Init();
+    init_lcd(TRUE, TRUE);
     //init_LCD();
     //clear_LCD();
     OpenSevenSegment();
-		OpenKeyPad();
+    OpenKeyPad();
     u8ADF = 0;
     /*//Buzzer
     GPIO_SetMode(PB, BIT11, GPIO_MODE_OUTPUT); 
@@ -215,26 +285,26 @@ void Init_ALL(void)
     GPIO_SET_DEBOUNCE_TIME(GPIO_DBCLKSRC_LIRC, GPIO_DBCLKSEL_256);
     GPIO_ENABLE_DEBOUNCE(PB, BIT15);*/
     //TIMER0
-    TIMER_Open(TIMER0, TMR0_OPERATING_MODE,200); // PERIODIC 200HZ = 5ms
+    TIMER_Open(TIMER0, TMR0_OPERATING_MODE, 200); // PERIODIC 200HZ = 5ms
     TIMER_EnableInt(TIMER0);
     NVIC_EnableIRQ(TMR0_IRQn);
     TIMER_Start(TIMER0);
     //TIMER1
-    TIMER_Open(TIMER1, TMR1_OPERATING_MODE,200); // PERIODIC 200HZ = 5ms
+    TIMER_Open(TIMER1, TMR1_OPERATING_MODE, 200); // PERIODIC 200HZ = 5ms
     TIMER_EnableInt(TIMER1);
     NVIC_EnableIRQ(TMR1_IRQn);
     TIMER_Start(TIMER1);
     //keypad interrupt
-    GPIO_SetMode(PA, (BIT0 | BIT1 | BIT2 |BIT3 | BIT4 | BIT5), GPIO_MODE_QUASI);
+    GPIO_SetMode(PA, (BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5), GPIO_MODE_QUASI);
     GPIO_EnableInt(PA, 0, GPIO_INT_LOW);
     GPIO_EnableInt(PA, 1, GPIO_INT_LOW);
-    GPIO_EnableInt(PA, 2, GPIO_INT_LOW);		
-    NVIC_EnableIRQ(GPAB_IRQn);   
-    NVIC_SetPriority(GPAB_IRQn,3);
+    GPIO_EnableInt(PA, 2, GPIO_INT_LOW);
+    NVIC_EnableIRQ(GPAB_IRQn);
+    NVIC_SetPriority(GPAB_IRQn, 3);
     GPIO_SET_DEBOUNCE_TIME(GPIO_DBCLKSRC_LIRC, GPIO_DBCLKSEL_256);
-    GPIO_ENABLE_DEBOUNCE(PA, (BIT0 | BIT1 | BIT2));	
+    GPIO_ENABLE_DEBOUNCE(PA, (BIT0 | BIT1 | BIT2));
     //ADC
-		ADC_Open(ADC, ADC_INPUT_MODE, ADC_OPERATION_MODE, ADC_CHANNEL_MASK);
+    ADC_Open(ADC, ADC_INPUT_MODE, ADC_OPERATION_MODE, ADC_CHANNEL_MASK);
     ADC_POWER_ON(ADC);
     ADC_EnableInt(ADC, ADC_ADF_INT);
     NVIC_EnableIRQ(ADC_IRQn);
@@ -248,165 +318,188 @@ void Init_ALL(void)
     // NVIC_EnableIRQ(UART02_IRQn);		              // enable Cortex-M0 NVIC interrupt for UART02
 }
 
-void judgeup(){
-    if(ball_x > 0 && ball_x < 0 + 32){
-				touchFlag = 1;
+void judgeup()
+{
+    if (ball_x > 0 && ball_x < 0 + 32)
+    {
+        touchFlag = 1;
         digit[1]++;
-        if(digit[1] == 10){
+        if (digit[1] == 10)
+        {
             digit[1] = 0;
         }
-				PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
+        PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
         PWM_ConfigOutputChannel(PWM1, PWM_CH0, 523, 80);
-				PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+        PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
     }
 }
-void judgedown(){
-    if(ball_x > pad_x && ball_x < pad_x + 32){
-				touchFlag = 1;
+void judgedown()
+{
+    if (ball_x > pad_x && ball_x < pad_x + 32)
+    {
+        touchFlag = 1;
         digit[0]++;
-        if(digit[0] == 10){
+        if (digit[0] == 10)
+        {
             digit[0] = 0;
         }
-				PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
+        PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
         PWM_ConfigOutputChannel(PWM1, PWM_CH0, 1, 80);
-				PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+        PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
     }
 }
 
 int main(void)
 {
 
-		int i = 0;
-		int dir;
-		Init_ALL();
-		// //LCD back light
-		// PD14 = 0;
-		srand(3020);
-		dir = rand() % 4;
-		if(dir == 0){
-			ball_dirx = 1;
-			ball_diry = 1;
-		}else if(dir == 1){
-			ball_dirx = -1;
-			ball_diry = 1;
-		}else if(dir == 2){
-			ball_dirx = 1;
-			ball_diry = -1;
-		}
-		else if(dir == 3){
-			ball_dirx = -1;
-			ball_diry = -1;
-		}
+    int i = 0;
+    int dir;
+    Init_ALL();
+    // //LCD back light
+    // PD14 = 0;
+    srand(3020);
+    dir = rand() % 4;
+    if (dir == 0)
+    {
+        ball_dirx = 1;
+        ball_diry = 1;
+    }
+    else if (dir == 1)
+    {
+        ball_dirx = -1;
+        ball_diry = 1;
+    }
+    else if (dir == 2)
+    {
+        ball_dirx = 1;
+        ball_diry = -1;
+    }
+    else if (dir == 3)
+    {
+        ball_dirx = -1;
+        ball_diry = -1;
+    }
 
-	 while(1) {
-		 
-        if(resultFlag == 0){ 
-            
-            if (cnt0_5ms % 20 == 0) 
-            {	
-								//draw_Bmp32x8(pad_x,pad_y ,BG_COLOR,BG_COLOR , pad);
-								//draw_Bmp32x8(0,0 ,BG_COLOR,BG_COLOR , pad);
+    while (1)
+    {
+
+        if (resultFlag == 0)
+        {
+
+            if (cnt0_5ms % 20 == 0)
+            {
+                //draw_Bmp32x8(pad_x,pad_y ,BG_COLOR,BG_COLOR , pad);
+                //draw_Bmp32x8(0,0 ,BG_COLOR,BG_COLOR , pad);
                 //draw ball
-								//draw_Bmp8x8(ball_x, ball_y,BG_COLOR,BG_COLOR , ball);
-								if(touchFlag == 0){
-									if(cnt0_100ms % 2 == 0)PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
-								}else if(touchFlag == 1){
-									PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
-									touchFlag = 0;
-								}
-								getVR();
-                
+                //draw_Bmp8x8(ball_x, ball_y,BG_COLOR,BG_COLOR , ball);
+                if (touchFlag == 0)
+                {
+                    if (cnt0_100ms % 2 == 0)
+                        PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
+                }
+                else if (touchFlag == 1)
+                {
+                    PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
+                    touchFlag = 0;
+                }
+                getVR();
+
                 //pinpon move
                 ball_x += ball_vx * ball_dirx;
                 ball_y += ball_vy * ball_diry;
-                if (ball_x > 120) {
+                if (ball_x > 120)
+                {
                     ball_x = 120;
                     ball_dirx = -1;
                     PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
-										PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+                    PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
                 }
-                if (ball_x < 0) {
+                if (ball_x < 0)
+                {
                     ball_x = 0;
                     ball_dirx = 1;
                     PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
-										PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+                    PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
                 }
-                if (ball_y > 56) {
-									judgedown();
+                if (ball_y > 56)
+                {
+                    judgedown();
                     ball_y = 56;
                     ball_diry = -1;
-										if(touchFlag == 0){
-											PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
-											PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
-										}
+                    if (touchFlag == 0)
+                    {
+                        PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
+                        PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+                    }
                 }
-                if (ball_y < 0) {
-										judgeup();
+                if (ball_y < 0)
+                {
+                    judgeup();
                     ball_y = 0;
                     ball_diry = 1;
-                    if(touchFlag == 0){
-											PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
-											PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
-										}
-										
+                    if (touchFlag == 0)
+                    {
+                        PWM_ConfigOutputChannel(PWM1, PWM_CH0, 2784, 80);
+                        PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+                    }
                 }
 
                 //draw LCD
                 //clear_LCD();
                 //draw ball
-								//draw_Bmp8x8(ball_x, ball_y,FG_COLOR,BG_COLOR , ball);
-								//draw_bitmap_in_buffer(ball,ball_x,ball_y,8,1,FG_COLOR);
-								//draw pad
-								//draw_Bmp32x8(pad_x,pad_y ,FG_COLOR,BG_COLOR , pad);
-								//draw_Bmp32x8(0,0 ,FG_COLOR,BG_COLOR , pad);
-								//draw_bitmap_in_buffer(pad,pad_x,pad_y,32,1,FG_COLOR);
-								//draw_bitmap_in_buffer(pad,0,0,32,1,FG_COLOR);
-								draw_bitmap_in_buffer(ball,ball_x,ball_y,8,1,FG_COLOR);
-								draw_bitmap_in_buffer(pad,pad_x,pad_y,32,1,FG_COLOR);
-								draw_bitmap_in_buffer(pad,0,0,32,1,FG_COLOR);
-								show_lcd_buffer();
+                //draw_Bmp8x8(ball_x, ball_y,FG_COLOR,BG_COLOR , ball);
+                //draw_bitmap_in_buffer(ball,ball_x,ball_y,8,1,FG_COLOR);
+                //draw pad
+                //draw_Bmp32x8(pad_x,pad_y ,FG_COLOR,BG_COLOR , pad);
+                //draw_Bmp32x8(0,0 ,FG_COLOR,BG_COLOR , pad);
+                //draw_bitmap_in_buffer(pad,pad_x,pad_y,32,1,FG_COLOR);
+                //draw_bitmap_in_buffer(pad,0,0,32,1,FG_COLOR);
+                draw_bitmap_in_buffer(ball, ball_x, ball_y, 8, 1, FG_COLOR);
+                draw_bitmap_in_buffer(pad, pad_x, pad_y, 32, 1, FG_COLOR);
+                draw_bitmap_in_buffer(pad, 0, 0, 32, 1, FG_COLOR);
+                show_lcd_buffer();
             }
-						
-            switch(KEY_Flag){
-                case 1:// 1
-                    KEY_Flag = 0;
-                    break;
-                case 2:// 2 speed up
-                    ball_vx++;
-                    ball_vy++;
-                    KEY_Flag = 0;
-                    break;
-                case 3:
-                    KEY_Flag = 0;
-                    break;
-                case 4:// 
-                    KEY_Flag = 0;
-										ball_vx=4;
-										ball_vy=4;
-                    break;
-                case 5:// 
-                    KEY_Flag = 0;
-										ball_vx=0;
-										ball_vy=0;
-                    break;
-                case 6:// 
-                    KEY_Flag = 0;
-                    break;
-                case 7:// =
-                    KEY_Flag = 0;
-                    break;
-                case 8:// speed down
-                    ball_vx/=2;
-                    ball_vy/=2;
-                    KEY_Flag = 0;
-                    break;
-                case 9:// 
-                    KEY_Flag = 0;
-                    break;
-                default:
-                    break;
+
+            switch (KEY_Flag)
+            {
+            case 1: // 1
+                KEY_Flag = 0;
+                break;
+            case 2: // 2 speed up
+                ball_vx++;
+                ball_vy++;
+                KEY_Flag = 0;
+                break;
+            case 3:
+                KEY_Flag = 0;
+                break;
+            case 4: //
+                KEY_Flag = 0;
+                ball_vx = 4;
+                ball_vy = 4;
+                break;
+            case 5: //
+                KEY_Flag = 0;
+                ball_vx = 0;
+                ball_vy = 0;
+                break;
+            case 6: //
+                KEY_Flag = 0;
+                break;
+            case 7: // =
+                KEY_Flag = 0;
+                break;
+            case 8: // speed down
+                ball_vx /= 2;
+                ball_vy /= 2;
+                KEY_Flag = 0;
+                break;
+            case 9: //
+                KEY_Flag = 0;
+                break;
+            default:
+                break;
             }
         }
-        
-  }
+    }
 }
