7ba7a40820149815dd8b13f4d0487a72d34253ae
benhsu0828 <97419397+benhsu0828@users.noreply.github.com>
2023-12-19 22:29:05 +0800

    Update LCD.h

--- onsite3/Q1/Library/Nu-LB-NUC140/Include/LCD.h
+++ onsite3/Q1/Library/Nu-LB-NUC140/Include/LCD.h
@@ -1,42 +1,32 @@
- 
 #define LCD_Xmax 128
 #define LCD_Ymax 64
 #define FG_COLOR 0xFFFF
 #define BG_COLOR 0x0000
 
-extern void init_LCD(void);
+extern void show_lcd_buffer(void);
 
-extern void clear_LCD(void);
+extern void clear_lcd_buffer(void);
 
-extern void printC(int16_t x, int16_t y, unsigned char ascii_code);
+extern void clear_lcd(void);
 
-extern void printC_5x7(int16_t x, int16_t y, unsigned char ascii_code);
+extern void init_lcd(uint8_t dynamic_update, uint8_t auto_clear);
 
-extern void print_Line(int8_t line, char text[]);
+extern void draw_bitmap_in_buffer(uint8_t bitmap[], int16_t x, int16_t y, int16_t bitmap_x_size, int16_t bitmap_y_size, uint16_t color);
 
-extern void printS(int16_t x, int16_t y, char text[]);
+extern void draw_pixel_in_buffer(int16_t x, int16_t y, uint16_t color);
 
-extern void printS_5x7(int16_t x, int16_t y, char text[]);
+extern void draw_line_in_buffer(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
 
-extern void draw_Bmp8x8(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp32x8(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp120x8(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp8x16(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x8(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x16(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x24(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x32(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x40(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x48(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp16x64(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp32x16(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp32x32(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp32x48(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp32x64(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
-extern void draw_Bmp64x64(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor, unsigned char bitmap[]);
+extern void draw_circle_in_buffer(int16_t xc, int16_t yc, int16_t r, uint16_t color, uint8_t isFill);
 
-extern void draw_Pixel(int16_t x, int16_t y, uint16_t fgColor, uint16_t bgColor);
+extern void draw_rectangle_in_buffer(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color, uint8_t isFill);
 
-extern void draw_LCD(unsigned char *buffer);
+extern void draw_triangle_in_buffer(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color, uint8_t isFill);
 
+extern void print_c_in_buffer(int16_t x, int16_t y, uint8_t size, unsigned char ascii_code, uint16_t color);
 
+extern void printf_s_in_buffer(int16_t x, int16_t y, uint8_t size, const char *format, ...);
+
+extern void printf_line_in_buffer(int8_t line, uint8_t size, const char *format, ...);
+
+extern uint8_t get_lcd_buffer_pixel(int16_t x, int16_t y);
